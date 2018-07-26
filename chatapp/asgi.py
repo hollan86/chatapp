51b9a84e11600900e2363203d107018391491ced
hollan haule <hollanhaule@hollans-MacBook-Pro.local>
2018-07-26 09:15:58 +0100

    code cleanup

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -2,6 +2,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatapp.settings.production")
 django.setup()
 application = get_default_application()
