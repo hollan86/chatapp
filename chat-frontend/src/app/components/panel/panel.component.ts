e321d2e5753fd57bac78d195ad690cb5040b4b1b
hollan haule <hollanhaule@hollans-MacBook-Pro.local>
2018-07-28 19:41:38 +0100

    code cleanup

--- chat-frontend/src/app/components/panel/panel.component.ts
+++ chat-frontend/src/app/components/panel/panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var anime :any;
 
 @Component({
   selector: 'app-panel',
@@ -10,6 +11,17 @@ export class PanelComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var path = anime.path('#motionPath path');
+    
+    var motionPath = anime({
+      targets: '#motionPath .el',
+      translateX: path('x'),
+      translateY: path('y'),
+      rotate: path('angle'),
+      easing: 'linear',
+      duration: 2000,
+      loop: true
+    });
   }
 
 }
